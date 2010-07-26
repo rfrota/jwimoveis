752880f91d4753df2b27312fa140902d824c56e7
renatobeserra <renatobeserra@07a62d37-c05c-b097-73ed-fe4b3239a804>
2010-07-26 06:46:45 +0000

    Models e Admin adicionados. Ajeitar nomes em ingles e o verbose name das classes filhas de  pessoa. Verificar campos que podem ser vazios.

---
+++ src/app/admin.py
@@ -0,0 +1,6 @@
+from src.app.models import Imovel,Inquilino,Proprietario
+from django.contrib import admin
+
+admin.site.register(Imovel)
+admin.site.register(Inquilino)
+admin.site.register(Proprietario)
--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 import datetime
 # Create your models here.
@@ -5,13 +6,17 @@ import datetime
 class Pessoa(models.Model):
     nome = models.CharField("Proprietário",
                             max_length=100,
-                            help_text='Nome do Proprietário do Imóvel')
+                            help_text='Nome do Proprietário do Imóvel',
+                            blank=False,
+                            null=False)
     cpf = models.IntegerField("CPF",
-                              max_length=11)
-    tel1 = models.IntegerField()
-    tel2 = models.IntegerField()
-    tel3 = models.IntegerField()
-
+                              max_length=11,
+                              primary_key=True)
+    tel1 = models.IntegerField(null=True)
+    tel2 = models.IntegerField(null=True)
+    tel3 = models.IntegerField(null=True)
+def __unicode__(self):
+        return self.nome
 class Inquilino(Pessoa):
     pass
 
@@ -21,25 +26,32 @@ class Proprietario(Pessoa):
     
 class Imovel(models.Model):
     dono = models.ForeignKey(Proprietario)
-    inquilino = models.ForeignKey(Inquilino)
+    inquilino = models.ForeignKey(Inquilino,
+                                  blank=True)
     ##casa/apartamento/escritorio/fazenda...
     tipo = models.CharField(max_length=50)
     estado = models.CharField(max_length=2)
     cidade = models.CharField(max_length=100)
-    bairro = models.CharFIeld(max_length=100)
+    bairro = models.CharField(max_length=100,
+                              blank=True)
     endereco = models.TextField("Endereço do Imóvel")
     ##tamanho da propriedade em metros quadrados
-    tamanho = models.IntegerField("Área Útil do Imóvel")
-    qtdDormitorios = models.IntegerField()    
+    tamanho = models.IntegerField("Área Útil do Imóvel",
+                                  null=True)
+    qtdDormitorios = models.IntegerField(null=True)    
     ##a seguir: true --> disponivel para venda/aluguel
-    aluguel = models.BooleanField()
-    venda = models.BooleanField()
+    aluguel = models.NullBooleanField(null=True)
+    venda = models.NullBooleanField(null=True)
     ##condiçao geral do imovel
-    condicao = models.CharField(max_length=50)
-    descricao = models.TextField("Descrição do Imóvel")
+    condicao = models.CharField(max_length=50,
+                                blank=True)
+    descricao = models.TextField("Descrição do Imóvel",
+                                 blank=True)
     ##valores-base para venda/aluguel
-    valorAluguel = models.IntegerField()
-    valorVenda = models.IntegerField()
+    valorAluguel = models.IntegerField(null=True)
+    valorVenda = models.IntegerField(null=True)
+    def __unicode__(self):
+        return '%s\n%s\n%s'(self.tipo,self.endereco,self.dono)
 
 
 
